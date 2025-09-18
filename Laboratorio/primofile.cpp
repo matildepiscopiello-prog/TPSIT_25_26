c64e18d9e4d204675452996fd993eb8fc9d78279
matildepiscopiello-prog <matilde.piscopiello@salveminialessano.edu.it>
2025-09-18 09:47:26 +0200

    Create primofile.cpp

---
+++ Laboratorio/primofile.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+    string nome, cognome;
+    nome = "Matilde";
+        cognome = "Piscopiello";
+   cout << "Ciao " << nome << " "<< cognome;
+    return 0;
+}
