d30c74c6dedede8f95de89c1e65455795ef1a66f
matildepiscopiello-prog <matilde.piscopiello@salveminialessano.edu.it>
2025-09-25 12:32:34 +0200

    Create uno.cpp
    dati due numeri fa la somma

---
+++ Laboratorio/uno.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int numero1, numero2, somma;
+
+    cout << "Inserisci il primo numero: ";
+    cin >> numero1;
+
+    cout << "Inserisci il secondo numero: ";
+    cin >> numero2;
+
+    somma = numero1 + numero2;
+
+    cout << "La somma dei due numeri è: " << somma << endl;
+
+    return 0;
+}
