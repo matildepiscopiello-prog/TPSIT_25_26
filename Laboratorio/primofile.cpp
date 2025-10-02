2d7934efe092bf75e96d0f7f0a7bb4ce1a392538
matildepiscopiello-prog <matilde.piscopiello@salveminialessano.edu.it>
2025-10-02 11:32:27 +0200

    Delete Laboratorio/primofile.cpp

--- Laboratorio/primofile.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-int main() {
-    string nome, cognome;
-    nome = "Matilde";
-        cognome = "Piscopiello";
-   cout << "Ciao " << nome << " "<< cognome;
-    return 0;
-}
